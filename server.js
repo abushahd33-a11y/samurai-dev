c852f3bcc07391fcd3b649e3469b74ffb1d117e9
abushahd33-a11y <abushahd33@gmail.com>
2026-04-20 07:22:41 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+app.use(express.json());
+
+app.post("/check", async (req, res) => {
+    try {
+        const { license, device } = req.body;
+
+        // تحقق بسيط (تقدر تطوره)
+        if (!license || !device) {
+            return res.json({ success: false });
+        }
+
+        // الاتصال الحقيقي (مخفي)
+        const response = await fetch("https://api.jsonbin.io/v3/b/69d1f6e5aaba882197c6ee95/latest", {
+            headers: {
+                "X-Master-Key": "YOUR_SECRET_KEY"
+            }
+        });
+
+        const data = await response.json();
+
+        res.json({
+            success: true,
+            data: data
+        });
+
+    } catch (e) {
+        res.json({ success: false });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running...");
+});
